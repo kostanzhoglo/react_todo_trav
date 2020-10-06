931e6696ff9f1e996550fd2cc3c83cf2ee02e7a6
kostanzhoglo <jeffreywithers@gmail.com>
2020-10-06 16:07:13 -0400

    Add TodoItem, and format that to show todo titles

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import TodoItem from './TodoItem';
 
 class Todos extends Component {
     render() {
         // console.log(this.props.todos)
         return this.props.todos.map((todo) => (
-        <h3>{ todo.title }</h3>
+            <TodoItem todo={todo}/>
         ));
     }
 }
