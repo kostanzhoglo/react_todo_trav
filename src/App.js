7a8a1566274f0d775dfaf1b21df7760d29d09c55
kostanzhoglo <jeffreywithers@gmail.com>
2020-10-05 22:56:52 -0400

    Add state to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
+import Todos from './components/Todos';
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+      }
+    ]
+  }
   render() {
     return (
       <div className="App">
-        <h1>My App</h1>
+        <Todos />
       </div>
     );
   }
