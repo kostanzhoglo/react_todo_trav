83323d2e5358f47fa64cb55bf4b4090399735c2b
kostanzhoglo <jeffreywithers@gmail.com>
2020-10-06 13:58:35 -0400

    Add props to Todos component, print them out

--- src/App.js
+++ src/App.js
@@ -9,13 +9,25 @@ class App extends Component {
         id: 1,
         title: 'Take out the trash',
         completed: false
+      },
+      {
+        id: 2,
+        title: 'Dinner with wife',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Meeting with boss',
+        completed: false
       }
     ]
   }
   render() {
     return (
       <div className="App">
-        <Todos />
+        <Todos
+          todos={this.state.todos}
+        />
       </div>
     );
   }
