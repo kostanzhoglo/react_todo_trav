e69b3aa7053b058097e724eeb209c4c74d6dd79a
kostanzhoglo <jeffreywithers@gmail.com>
2020-10-09 16:36:14 -0400

    Add checkbox and finish 'complete' styling

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class TodoItem extends Component {
+    getStyle = () => {
+        return {
+            textDecoration: this.props.todo.completed ? 'line-through' : 'none',
+            backgroundColor: '#84f3a2',
+            padding: '10px',
+            borderBottom: '1px #fff dotted'
+        }
+    }
+
     render() {
         return (
-            <div>
-                <p>{ this.props.todo.id }. { this.props.todo.title }</p>
+            <div style={this.getStyle()}>
+                <p>
+                    <input type="checkbox" /> {' '}
+                    { this.props.todo.id }. { this.props.todo.title }
+                </p>
             </div>
         );
     }
+}
+
+TodoItem.propTypes = {
+    todo: PropTypes.object.isRequired 
 }
 
 export default TodoItem;
