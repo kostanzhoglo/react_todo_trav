12023aac705059dfdeab8cd50e33e1581e714600
kostanzhoglo <jeffreywithers@gmail.com>
2020-10-11 21:21:16 -0400

    Add Delete button and styling

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -18,6 +18,7 @@ export class TodoItem extends Component {
                 <p>
                     <input type="checkbox" onChange={this.props.toggleComplete.bind(this, id)}/> {' '}
                     { title }
+                    <button style={btnStyle}>x</button>
                 </p>
             </div>
         );
@@ -26,6 +27,16 @@ export class TodoItem extends Component {
 
 TodoItem.propTypes = {
     todo: PropTypes.object.isRequired 
+}
+
+const btnStyle = {
+    background: '#ff0000',
+    color: '#fff',
+    border: 'none',
+    padding: '5px 9px',
+    borderRadius: '50%',
+    cursor: 'pointer',
+    float: 'right'
 }
 
 export default TodoItem;
